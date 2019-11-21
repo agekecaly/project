7885155bf0036dc5cd4a97865dd6b0aaf9086791
Ageke Caly <agekecaly@gmail.com>
2019-11-21 08:42:56 +0300

    initial commit

---
+++ frontend/views/site/about.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>The company started in the year 2015 November 2.</p>
+
+    <!-- <code><?= __FILE__ ?></code> -->
+</div>
